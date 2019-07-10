03d219b4f6f6e8fb9cba3ce14354b0ac61b79232
Andrew Culver <andrew.culver@gmail.com>
2019-07-10 18:09:20 -0400

    Allow versions of Rails newer than 5.

--- lib/filterrific.rb
+++ lib/filterrific.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-if Rails::VERSION::MAJOR != 5
-  raise "\n\nThis version of Filterrific only works with Rails 5.x.\nPlease see the Filterrific README for the correct version of Filterrific to use with your version of Rails!\n\n"
+if Rails::VERSION::MAJOR < 5
+  raise "\n\nThis version of Filterrific only works with Rails 5.x and newer.\nPlease see the Filterrific README for the correct version of Filterrific to use with your version of Rails!\n\n"
 end
 
 require 'filterrific/version'
